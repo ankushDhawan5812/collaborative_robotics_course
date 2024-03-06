c5dcf62565de18948423c6b6bad1f4aadd94b08a
adityadutt09 <adityadutt09@gmail.com>
2024-03-05 19:51:10 -0800

    Adding launch file

---
+++ locobot_autonomy/launch/initial_state_launch.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # Launching other launch files
+    launch_file1 = os.path.join(os.path.dirname(__file__), '/home/adityadutt09/interbotix_ws/src/interbotix_ros_rovers/interbotix_ros_xslocobots/interbotix_xslocobot_sim/launch/xslocobot_gz_classic.launch.py')
+    ld.add_action(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(launch_file1),
+        launch_arguments={'robot_model': 'locobot_wx250s', 'use_sim_time': 'true'}.items()
+    ))
+
+    launch_file2 = os.path.join(os.path.dirname(__file__), 'spawn_cube_launch.py')
+    ld.add_action(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(launch_file1)
+    ))
+
+    # Launching action server
+    ld.add_action(Node(
+        package='locobot_autonomy',
+        executable='move_locobot_base_action_server',
+        name='movebase_action_server',
+        output='screen',
+        #parameters=[{'param_name': 'param_value'}]  # Optional parameters
+    ))
+
+    # Launching action client
+    ld.add_action(Node(
+        package='locobot_autonomy',
+        executable='visual_block_perception_srv',
+        name='Matching_Pix_to_Ptcld',
+        output='screen',
+        # parameters=[{'param_name': 'param_value'}]  # Optional parameters
+    ))
+
+    # Launching regular node
+    ld.add_action(Node(
+        package='locobot_autonomy',
+        executable='move_arm_action_srv',
+        name='action_server_node',
+        output='screen',
+        # parameters=[{'param_name': 'param_value'}]  # Optional parameters
+    ))
+
+    return ld
